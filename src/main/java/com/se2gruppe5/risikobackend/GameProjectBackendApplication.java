1ca0b168ebe191b84921ca3aa20eb587275d59a1
Markus-M-AAU <markusmue@edu.aau.at>
2025-03-17 16:45:45 +0100

    Set up project

---
+++ src/main/java/com/se2gruppe5/risikobackend/GameProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.se2gruppe5.risikobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se2gruppe5/risikobackend/GameProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se2gruppe5.risikobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
