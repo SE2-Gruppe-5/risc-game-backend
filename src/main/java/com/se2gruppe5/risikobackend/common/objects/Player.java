94397bd98a244662059826a0f9dfe63d8cf28dab
Rene <rene.bierbaumer04@gmail.com>
2025-05-10 20:05:41 +0200

    Objects created

--- src/main/java/com/se2gruppe5/risikobackend/common/objects/Player.java
+++ src/main/java/com/se2gruppe5/risikobackend/common/objects/Player.java
@@ -1,6 +1,14 @@
 package com.se2gruppe5.risikobackend.common.objects;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-public record Player(UUID uuid, String name) {
+public record Player(UUID uuid, String name, List<Card> cards) {
+    public Player(UUID uuid, String name){
+        this(uuid, name, new ArrayList<Card>());
+    }
+
+
 }
--- src/main/java/com/se2gruppe5/risikobackend/game/objects/Game.java
+++ src/main/java/com/se2gruppe5/risikobackend/game/objects/Game.java
@@ -1,16 +1,22 @@
 package com.se2gruppe5.risikobackend.game.objects;
 
 import com.se2gruppe5.risikobackend.common.objects.Player;
+import com.se2gruppe5.risikobackend.common.objects.Territory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-public record Game(UUID uuid, ConcurrentHashMap<UUID, Player> players) {
+public record Game(UUID uuid, ConcurrentHashMap<UUID, Player> players, List<Territory> territories) {
     public Game(UUID uuid) {
-        this(uuid, new ConcurrentHashMap<>());
+        this(uuid, new ConcurrentHashMap<>(), new ArrayList<Territory>());
     }
-
+    public Game(UUID uuid, ConcurrentHashMap<UUID, Player> players) {
+        this(uuid, players, new ArrayList<Territory>());
+    }
     public void start() {
         //TODO
     }
+
 }
