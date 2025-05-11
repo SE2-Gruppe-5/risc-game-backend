4f2d85d42f6d1c4f7c9661c63df3553f6d62da4b
Markus-M-AAU <markusmue@edu.aau.at>
2025-05-11 02:40:27 +0200

    Removed unused imports

--- src/main/java/com/se2gruppe5/risikobackend/chat/controllers/ChatController.java
+++ src/main/java/com/se2gruppe5/risikobackend/chat/controllers/ChatController.java
@@ -4,7 +4,6 @@ import com.se2gruppe5.risikobackend.chat.messages.ChatMessage;
 import com.se2gruppe5.risikobackend.sse.services.SseBroadcastService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/com/se2gruppe5/risikobackend/game/controllers/GameController.java
+++ src/main/java/com/se2gruppe5/risikobackend/game/controllers/GameController.java
@@ -10,10 +10,8 @@ import com.se2gruppe5.risikobackend.game.messages.UpdatePlayersMessage;
 import com.se2gruppe5.risikobackend.game.services.GameService;
 
 import com.se2gruppe5.risikobackend.sse.services.SseBroadcastService;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
