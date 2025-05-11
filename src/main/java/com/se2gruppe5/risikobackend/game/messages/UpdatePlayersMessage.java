d7323e2d0fb3688c24bc32a2ae9893ed8f9e2d54
Webbiii <webbiii@webbiii.cc>
2025-05-11 03:37:38 +0200

    Can now use either Map or List of Players

--- src/main/java/com/se2gruppe5/risikobackend/game/messages/UpdatePlayersMessage.java
+++ src/main/java/com/se2gruppe5/risikobackend/game/messages/UpdatePlayersMessage.java
@@ -4,10 +4,17 @@ import com.se2gruppe5.risikobackend.common.objects.Player;
 import com.se2gruppe5.risikobackend.sse.Message;
 import com.se2gruppe5.risikobackend.sse.MessageType;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
-public record UpdatePlayersMessage(ConcurrentHashMap<UUID, Player> players) implements Message {
+public record UpdatePlayersMessage(Map<UUID, Player> players) implements Message {
+    public UpdatePlayersMessage(List<Player> players) {
+        this(players.stream().collect(Collectors.toMap(Player::getUuid, UnaryOperator.identity())));
+    }
+
     @Override
     public MessageType getType() {
         return MessageType.UPDATE_PLAYERS;
