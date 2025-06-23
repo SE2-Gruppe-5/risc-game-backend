447830560ab7a695b1402d3284ee7c218bc66fd6
Rene <rene.bierbaumer04@gmail.com>
2025-06-23 21:28:03 +0200

    implemented PlayWon Message

---
+++ src/main/java/com/se2gruppe5/risikobackend/game/messages/PlayerWonMessage.java
@@ -0,0 +1,14 @@
+package com.se2gruppe5.risikobackend.game.messages;
+
+import com.se2gruppe5.risikobackend.sse.Message;
+import com.se2gruppe5.risikobackend.sse.MessageType;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public record PlayerWonMessage(UUID winner) implements Message {
+    @Override
+    public MessageType getType() {
+        return MessageType.PLAYER_WON;
+    }
+}
